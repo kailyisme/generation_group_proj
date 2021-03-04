b1ce9b08d6d0600e610e2b947730b74c77b1d576
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2021-03-04 11:44:03 +0000

    cleared up the sizes

--- src/db_handlers/tables.sql
+++ src/db_handlers/tables.sql
@@ -1,41 +1,38 @@
 CREATE TABLE IF NOT EXISTS store (
     uuid UUID PRIMARY KEY,
-    location VARCHAR(255)
+    location VARCHAR(255) NOT NULL
     );
-
--- CREATE TABLE IF NOT EXISTS product_size (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(255)
---     );
 
 
 CREATE TABLE IF NOT EXISTS product_type (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255) NOT NULL
     );
+
+
+CREATE TYPE SIZE AS ENUM('Small', 'Regular', 'Large');
 
 
 CREATE TABLE IF NOT EXISTS product (
     uuid UUID PRIMARY KEY,
     type_id INTEGER REFERENCES product_type(id),
-    name VARCHAR(255),
-    size VARCHAR(255),
-    -- size_id INTEGER REFERENCES product_size(id),
-    price NUMERIC(6,2)
+    name VARCHAR(255) NOT NULL,
+    size SIZE NOT NULL DEFAULT 'Regular',
+    price NUMERIC(6,2) NOT NULL
     );
 
 
 CREATE TABLE IF NOT EXISTS transaction (
     uuid UUID PRIMARY KEY,
-    datetime TIMESTAMP,
-    store_uuid UUID REFERENCES store(uuid),
-    payment_type VARCHAR(4),
-    total_amount NUMERIC(6,2)
+    datetime TIMESTAMP NOT NULL,
+    store_uuid UUID REFERENCES store(uuid) NOT NULL,
+    payment_type VARCHAR(4) NOT NULL,
+    total_amount NUMERIC(6,2) NOT NULL
     );
 
 
 CREATE TABLE IF NOT EXISTS basket (
     id UUID PRIMARY KEY,
-    transaction_uuid UUID REFERENCES transaction(uuid),
-    product_uuid UUID REFERENCES product(uuid)
+    transaction_uuid UUID REFERENCES transaction(uuid) NOT NULL,
+    product_uuid UUID REFERENCES product(uuid) NOT NULL
     );
