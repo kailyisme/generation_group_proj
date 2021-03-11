b109da90c42c1a8b4ee91a288ae733e94fa8bb94
silicone_girl <kaily.a.newman@gmail.com>
2021-03-11 18:27:47 +0000

    Ready and done to merge

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -3,7 +3,7 @@ import csv
 filename = "./data/2021-02-23-isle-of-wight.csv"
 
 
-def extract_csv(filename):
+def extract_csv(filename=filename):
     csv_data = []
     with open(filename) as file:
         data = csv.DictReader(
