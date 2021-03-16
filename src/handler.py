312e11297dad9f91f3f4f89996d8aa3bd7ed6958
silicone_girl <kaily.a.newman@gmail.com>
2021-03-16 15:35:37 +0000

    connected to redshift, to solve enumerate type

--- src/handler.py
+++ src/handler.py
@@ -7,11 +7,14 @@ from src.etl.load import load_db
 from src.db_handlers import db_init
 
 def execute(event, context):
-    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-    key = event["Records"][0]["s3"]["object"]["key"]
-    s3_res = boto3.resource("s3")
-    s3_object = s3_res.Object(bucket_name, key)
-    raw = s3_object.get()["Body"].read().decode("utf-8").splitlines()
+    try:
+        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+        key = event["Records"][0]["s3"]["object"]["key"]
+        s3_res = boto3.resource("s3")
+        s3_object = s3_res.Object(bucket_name, key)
+        raw = s3_object.get()["Body"].read().decode("utf-8").splitlines()
+    except Exception as e:
+        print(e)
 
     df = ex.extract_csv(raw)
     df = tr.transform_run(df)
