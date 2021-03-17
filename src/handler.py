cb7ba799347790e8e5c6227d8a98d0a14ff0355d
Hamid Munir <Hamidmunir9@gmail.com>
2021-03-17 22:50:59 +0000

    testing env

--- src/handler.py
+++ src/handler.py
@@ -6,6 +6,9 @@ from src.etl import transform as tr
 from src.etl.load import load_db
 from src.db_handlers import db_init
 
+from dotenv import load_dotenv
+from dotenv.main import get_key
+
 def execute(event, context):
     try:
         bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
@@ -16,9 +19,23 @@ def execute(event, context):
     except Exception as e:
         print(e)
 
+    HOST = os.environ.get("POSTGRES_HOST")
+    USER = os.environ.get("POSTGRES_USER")
+    PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    DB = os.environ.get("POSTGRES_DB")
+    PORT = os.environ.get("POSTGRES_PORT")
+    DB1 = os.environ["POSTGRES_DB"]
+    print(db1)
+    
+    print(PORT)
+    print(DB)
+    print(PASSWORD)
+    print(USER)
+    print(HOST)
+    
     df = ex.extract_csv(raw)
     df = tr.transform_run(df)
-    
+    print(df[1])
     conn = db_init.init_db()
     load_db(conn, df)
     
